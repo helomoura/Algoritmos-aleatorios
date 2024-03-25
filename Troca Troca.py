0462681f0912e597b331490cacf27738c2613e48
helomoura <161479704+helomoura@users.noreply.github.com>
2024-03-25 11:12:44 -0300

    Add files via upload

---
+++ Troca Troca.py
@@ -0,0 +1,11 @@
+def Troca_Troca():
+    valor_A = input("Informe o valor de A: ")
+    valor_B = input("Informe o valor de B: ")
+    temp_A = valor_A
+    temp_B = valor_B
+    valor_A = temp_B
+    valor_B = temp_A
+    print(f"valor de A {valor_A} e valor de B {valor_B}")
+
+Troca_Troca()
+
---
+++ TwoFer.py
@@ -0,0 +1,10 @@
+def TwoFer():
+    nome = input("Informe o seu nome:")
+    if not nome:
+        nome = "você"
+    else:
+        pass
+
+    print(f"Um para {nome}, um para mim.")
+
+TwoFer()
